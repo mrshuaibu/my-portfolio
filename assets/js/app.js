83e4b3d4a4875d846eaa4961123b069bc29ba6bf
Michael Shuaibu <shuaibumikel@gmail.com>
2024-11-27 09:35:11 -0600

    toggle in responsive mode added in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,5 +5,5 @@ const getInTouchButton = document.querySelector('.get-in-touch-button');
 
 menuIcon.addEventListener('click', function() {
     menuIcon.classList.toggle('active');
-    getInTouchButton.classList.toggle('active'); // Toggles visibility of the "Get in touch" button
+    getInTouchButton.classList.toggle('active'); 
 });
