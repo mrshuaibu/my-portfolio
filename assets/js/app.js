ba0a4e494704ba45afb39384d4e2b4e802e6aadb
Michael Shuaibu <shuaibumikel@gmail.com>
2024-11-27 12:23:09 -0600

    event listeners updated in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,31 @@
 const menuIcon = document.querySelector('.menu-icon');
 const getInTouchButton = document.querySelector('.get-in-touch-button');
 
-menuIcon.addEventListener('click', function() {
+function closeActiveElements(event) {
+    menuIcon.classList.remove('active');
+    getInTouchButton.classList.remove('active');
+    removeEventListeners();
+}
+
+function addEventListeners() {
+    document.addEventListener('click', closeActiveElements);
+    document.addEventListener('scroll', closeActiveElements);
+}
+
+function removeEventListeners() {
+    document.removeEventListener('click', closeActiveElements);
+    document.removeEventListener('scroll', closeActiveElements);
+}
+
+menuIcon.addEventListener('click', function (event) {
     menuIcon.classList.toggle('active');
-    getInTouchButton.classList.toggle('active'); 
+    getInTouchButton.classList.toggle('active');
+
+    if (menuIcon.classList.contains('active') || getInTouchButton.classList.contains('active')) {
+        addEventListeners();
+    } else {
+        removeEventListeners();
+    }
+
+    event.stopPropagation();
 });
